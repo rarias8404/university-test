e34a1697a2874699563f7efddeae7bde57cd68d4
Renier <renier.arias@gmail.com>
2020-11-20 11:31:36 -0500

    database

--- src/components/Students/StudentsPage.js
+++ src/components/Students/StudentsPage.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import {Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from "react-router-dom";
 
 class StudentsPage extends Component {
   render() {
     return (
       <div className="container">
-        <h4>Students Page</h4>
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem active>Students</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Students</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row row-content">
+          <div className="col-12 col-md-6">
+            <h2>Students</h2>
+          </div>
+        </div>
       </div>
     );
   }
