8c3fdab0089cff3dd29336b512e3b43d2206433a
Renier <renier.arias@gmail.com>
2020-11-23 14:11:30 -0500

    group redux

--- src/redux/actions/groupActions.js
+++ src/redux/actions/groupActions.js
@@ -7,13 +7,43 @@ export const getGroupsSuccess = groups => ({
   groups
 });
 
-export const getStudentsFailed = error => ({
+export const getGroupsFailed = error => ({
   type: actionTypes.GET_GROUPS_FAILED,
   error: error.message
 });
 
 export const groupsLoading = () => ({
   type: actionTypes.GROUPS_LOADING
+});
+
+export const createGroupSuccess = group => ({
+  type: actionTypes.CREATE_GROUP_SUCCESS,
+  group
+});
+
+export const createGroupFailed = error => ({
+  type: actionTypes.CREATE_GROUP_FAILED,
+  error: error.message
+});
+
+export const updateGroupSuccess = group => ({
+  type: actionTypes.UPDATE_GROUP_SUCCESS,
+  group
+});
+
+export const updateGroupFailed = error => ({
+  type: actionTypes.UPDATE_GROUP_FAILED,
+  error: error.message
+});
+
+export const deleteGroupSuccess = id => ({
+  type: actionTypes.DELETE_GROUP_SUCCESS,
+  id
+});
+
+export const deleteGroupFailed = error => ({
+  type: actionTypes.DELETE_GROUP_FAILED,
+  error: error.message
 });
 
 export function getGroups () {
@@ -24,8 +54,51 @@ export function getGroups () {
         dispatch(getGroupsSuccess(response.data))
       })
       .catch(error => {
-        dispatch(getStudentsFailed(error));
+        dispatch(getGroupsFailed(error));
         toastr.error('', error.message);
+      })
+  }
+}
+
+export function saveGroup(group) {
+  return dispatch => {
+    dispatch(groupsLoading());
+    if (group.id === null) {
+      groupApi.addGroup(group)
+        .then(() => {
+          dispatch(createGroupSuccess(group));
+          toastr.success('', 'Group created successfully');
+        })
+        .catch(error => {
+          dispatch(createGroupFailed(error));
+          toastr.error('', 'Error creating group');
+        })
+    }
+    else {
+      groupApi.updateGroup(group)
+        .then(() => {
+          dispatch(updateGroupSuccess(group));
+          toastr.success('', 'Group updated successfully');
+        })
+        .catch(error => {
+          dispatch(updateGroupFailed(error));
+          toastr.error('', 'Error updating group');
+        })
+    }
+  }
+}
+
+export function deleteGroup(id) {
+  return dispatch => {
+    dispatch(groupsLoading());
+    groupApi.deleteGroup(id)
+      .then(() => {
+        dispatch(deleteGroupSuccess(id));
+        toastr.success('', 'Group deleted successfully');
+      })
+      .catch(error => {
+        dispatch(deleteGroupFailed(error));
+        toastr.error('', 'Error deleting group');
       })
   }
 }
--- src/redux/reducers/groupReducer.js
+++ src/redux/reducers/groupReducer.js
@@ -15,6 +15,26 @@ export const groups = (state = initialState, action) => {
       return {...state, loading: false, groups: [], loadError: action.error}
     case actionTypes.GROUPS_LOADING:
       return {...state, loading: true, groups: [], loadError: null}
+    case actionTypes.CREATE_GROUP_SUCCESS:
+      const group = {...action.group};
+      group.id = state.groups.length + 1;
+      return {...state, loading: false, groups: [...state.groups, group], dataError: null}
+    case actionTypes.UPDATE_GROUP_SUCCESS:
+      const newState = [...state.groups];
+      newState.forEach(group => {
+        if (parseInt(group.id, 10) === parseInt(action.group.id, 10)) {
+          Object.assign(group, action.group);
+        }
+      });
+      return {...state, loading: false, groups: newState, dataError: null};
+    case actionTypes.CREATE_GROUP_FAILED:
+    case actionTypes.UPDATE_GROUP_FAILED:
+    case actionTypes.DELETE_GROUP_FAILED:
+      return {...state, loading: false, dataError: action.error}
+    case actionTypes.DELETE_GROUP_SUCCESS:
+      let groups = [...state.groups];
+      groups = groups.filter(group => parseInt(group.id) !== parseInt(action.id));
+      return {...state, loading: false, groups, dataError: null}
     default:
       return state;
   }
