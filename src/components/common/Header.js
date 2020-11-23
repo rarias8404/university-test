05ea681da2fbc78b7b814d832af50c6471a3b769
Renier <renier.arias@gmail.com>
2020-11-23 12:34:34 -0500

    refactoring Header component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {
   Nav,
   Navbar,
@@ -9,65 +9,53 @@ import {
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isNavOpen: false,
-    }
-    this.toggleNav = this.toggleNav.bind(this);
+const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
   }
 
-  toggleNav() {
-    this.setState(prevState => ({
-      isNavOpen: !prevState.isNavOpen
-    }));
-  }
-
-  render() {
-    const { isNavOpen } = this.state;
-    return (
-      <React.Fragment>
-        <Navbar dark expand="md">
-          <div className="container">
-            <NavbarToggler onClick={this.toggleNav} />
-            <NavbarBrand className="mr-auto" href="/">
-              <img src="logo512.png" height="30" width="41" alt="University Test"/>
-            </NavbarBrand>
-            <Collapse isOpen={isNavOpen} navbar>
-              <Nav navbar>
-                <NavItem>
-                  <NavLink className="nav-link" to="/home">
-                    <span className="fa fa-university fa-lg"/> Home
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/students">
-                    <span className="fa fa-user fa-lg"/> Students
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/groups">
-                    <span className="fa fa-list fa-lg"/> Groups
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/about-us">
-                    <span className="fa fa-info fa-lg"/> About Us
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/contact-us">
-                    <span className="fa fa-address-card fa-lg"/> Contact Us
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </div>
-        </Navbar>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Navbar dark expand="md">
+        <div className="container">
+          <NavbarToggler onClick={toggleNav} />
+          <NavbarBrand className="mr-auto" href="/">
+            <img src="logo512.png" height="30" width="41" alt="University Test"/>
+          </NavbarBrand>
+          <Collapse isOpen={isNavOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/home">
+                  <span className="fa fa-university fa-lg"/> Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/students">
+                  <span className="fa fa-user fa-lg"/> Students
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/groups">
+                  <span className="fa fa-list fa-lg"/> Groups
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/about-us">
+                  <span className="fa fa-info fa-lg"/> About Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/contact-us">
+                  <span className="fa fa-address-card fa-lg"/> Contact Us
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </div>
+      </Navbar>
+    </React.Fragment>
+  );
 }
 
 export default Header;
