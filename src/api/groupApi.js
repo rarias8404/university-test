a5e195ea0841f2a7110cfb3c458166ee2fdbacda
Renier <renier.arias@gmail.com>
2020-11-23 13:56:05 -0500

    update group api

--- src/api/groupApi.js
+++ src/api/groupApi.js
@@ -4,3 +4,15 @@ import {baseUrl} from "./baseUrl";
 export const getGroups = () => {
   return axios.get(`${baseUrl}groups`);
 }
+
+export const addGroup = group => {
+  return axios.post(`${baseUrl}groups`, group);
+};
+
+export const updateGroup = group => {
+  return axios.patch(`${baseUrl}groups/${group.id}`, group);
+};
+
+export const deleteGroup = id => {
+  return axios.delete(`${baseUrl}groups/${id}`)
+};
